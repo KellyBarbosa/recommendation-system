fa3363abd81d2a563e606ce865fa6862d9533124
KellyBarbosa <kelly.189@hotmail.com>
2022-11-13 03:21:42 -0300

    Adicionado o script para fechar notificação

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -10,8 +10,9 @@ router.get("/", (req, res) => {
 
 router.post("/register", (req, res) => {
   let email = req.body.email;
-  console.log("E-mail: " + email);
+
   if (email.length > 0 && email !== undefined) {
+    console.log("E-mail: " + email);
     req.flash("message", "E-mail cadastrado com sucesso!");
     req.flash("status", "success");
     res.redirect("/");
